c7ac221dc1478c13b3750e866e2fe35a9438fe65
nrm461 <nrm461@gmail.com>
2026-07-10 07:50:20 -0400

    [auto-build] no muted-autoplay on mobile players

--- js/main.js
+++ js/main.js
@@ -113,7 +113,9 @@
 			var id = f.getAttribute('data-vimeo');
 			var params = 'badge=0&autopause=0&player_id=0&title=0&byline=0&portrait=0' +
 				'&vimeo_logo=0&pip=0&cc=0&transcript=0&airplay=0&chromecast=0' +
-				'&watch_full_video=0&dnt=1&autoplay=1';
+				'&watch_full_video=0&dnt=1';
+			/* mobile browsers only autoplay muted (with an unmute pill) — skip autoplay there */
+			if(!document.body.classList.contains('mobile')) params += '&autoplay=1';
 			var box = f.querySelector('div');
 			box.innerHTML = '<iframe src="https://player.vimeo.com/video/' + id + '?' + params +
 				'" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; encrypted-media" title="video"></iframe>';
